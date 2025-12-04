3a8c163943c991bf0bc520169ec2a728cc3d076a
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-12-04 18:38:11 +0100

    Pianos without Create

--- public_pages/views.py
+++ public_pages/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'public_pages/index.html')
+
+
+def about_us(request):
+    return render(request, 'public_pages/about_us.html')
