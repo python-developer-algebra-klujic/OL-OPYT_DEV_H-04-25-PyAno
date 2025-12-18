ebb3dbe05433832edd02a88fa635440727a469f2
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-12-18 18:35:36 +0100

    Added Django Crispy forms

--- project_core/settings.py
+++ project_core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'crispy_tailwind',
+
     'pianos',
     'public_pages',
     'chords',
@@ -124,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+# CRISPY_ALLOWED_TEMPLATE_PACKS = 'tailwind'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+# CRISPY_TEMPLATE_PACK = 'tailwind'
